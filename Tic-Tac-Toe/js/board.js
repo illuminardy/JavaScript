0454c931e42a4e69cc27feee091e63446f2e9c0e
Illuminardy <yanardy.sanchez@gmail.com>
2015-10-29 16:35:38 -0400

    Refactoring the implementation based on the fact that size will always be 3x3.

--- Tic-Tac-Toe/js/board.js
+++ Tic-Tac-Toe/js/board.js
@@ -1,83 +1,41 @@
 document.addEventListener('DOMContentLoaded', initializeBoard);
 
-// Initial representation of the board
-var board = [
-	[" ", " ", " "],
-	[" ", " ", " "],
-	[" ", " ", " "]
-];
+// Declarations
+var currentPlayer = "X",
+	currentColor = "blue",
+	board = [];
 
 function initializeBoard(){
-	var gameBoardDiv = document.getElementById("gameBoard");
-	
-	for (var i = 0; i < board.length; i++){
-		// Create parent divs per row
-		var parentDiv = document.createElement("div");
-		parentDiv.className = "row";
-		document.body.appendChild(parentDiv);
-		
-		for (var j = 0; j < board[0].length; j++){
-			
-			// Create child divs
-			var childDiv = document.createElement("div");
-			childDiv.className = "square";
-			childDiv.innerHTML = board[i][j];
-			childDiv.onclick = playerClickedCellFunc;
-			
-			var rowAttr = document.createAttribute("rowNum");
-			rowAttr.value = i;   
-			
-			var colAttr = document.createAttribute("colNum");
-			colAttr.value = j; 
-			
-			childDiv.setAttributeNode(rowAttr);
-			childDiv.setAttributeNode(colAttr);
-			parentDiv.appendChild(childDiv);
-		}
-		gameBoardDiv.appendChild(parentDiv);
+	board = [["", "", ""],["", "", ""],["", "", ""]];
+	var tds = document.querySelectorAll(".square");
+	for (var i = 0; i < tds.length; i++){
+		tds[i].onclick = playerClickedCellFunc;
 	}
-}
-
-// Player objects
-var player1 = {
-	name: "Anny",
-	symbol: "X"
-};
-
-var player2 = {
-	name: "Yanardy",
-	symbol: "O"
-};
-
-var currentPlayer = player1;
-var currentColor = "blue";
-
-function switchCurrentPlayer(){
-	var currentPlayerDiv = document.getElementById("currentPlayer");
-	if (currentPlayer == player1){
-		currentPlayer = player2;
-		currentColor = "red"
-	} else {
-		currentPlayer = player1;
-		currentColor = "blue"
-	}
-	currentPlayerDiv.innerHTML = "Current Player: " + currentPlayer.name;
 }
 
 function playerClickedCellFunc(){
-	if (this.innerHTML === " "){
-		this.innerHTML = currentPlayer.symbol;
+	if (this.innerHTML === ""){
+		this.innerHTML = currentPlayer;
 		this.style.color = currentColor;
-		var row = this.attributes.rowNum.value;
-		var col = this.attributes.colNum.value;
-		board[row][col] = currentPlayer.symbol;
+
+		board[row][col] = currentPlayer;
 		if (playerWon()) {
-			alert(currentPlayer.name + " " + "has won the game!!!");
+			alert("has won the game!!!");
 			return;
 		}
 		switchCurrentPlayer();
 	}
-};
+}
+
+function switchCurrentPlayer(){
+	if (currentPlayer == "X"){
+		currentPlayer = "O";
+		currentColor = "red"
+	} else {
+		currentPlayer = "X";
+		currentColor = "blue"
+	}
+}
 
 function playerWon(){
 	var consecutiveSymbols = 0;
