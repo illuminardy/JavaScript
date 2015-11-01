afa5c35bcf51cd8ecf8b3e44bc343e8d31d78d54
illuminardy <Yanardy.Sanchez@gmail.com>
2015-11-01 12:14:02 -0500

    Wrapped entire js code in a function to provide private methods. Clened up method for checking if a player has won

--- Tic-Tac-Toe/js/board.js
+++ Tic-Tac-Toe/js/board.js
@@ -1,80 +1,68 @@
-document.addEventListener('DOMContentLoaded', initializeBoard);
-
-// Declarations
-var currentPlayer = "X",
+var TicTacToe = (function(){
+	// Declarations
+	var currentPlayer = "X",
 	currentColor = "blue",
 	board = [];
 
-function initializeBoard(){
-	board = [["", "", ""],["", "", ""],["", "", ""]];
-	var tds = document.querySelectorAll(".square");
-	for (var i = 0; i < tds.length; i++){
-		tds[i].onclick = playerClickedCellFunc;
-	}
-}
-
-function playerClickedCellFunc(){
-	if (this.innerHTML === ""){
-		this.innerHTML = currentPlayer;
-		this.style.color = currentColor;
-		var row = this.parentNode.id;
-		var col = this.id;
-
-		board[row][col] = currentPlayer;
-		if (playerWon()) {
-			alert("has won the game!!!");
-			return;
+	function initializeBoard(){
+		board = [["", "", ""],["", "", ""],["", "", ""]];
+		var tds = document.body.querySelectorAll(".square");
+		for (var i = 0; i < tds.length; i++){
+			tds[i].onclick = playerClickedCellFunc;
 		}
-		switchCurrentPlayer();
 	}
-}
 
-function switchCurrentPlayer(){
-	if (currentPlayer == "X"){
-		currentPlayer = "O";
-		currentColor = "red"
-	} else {
-		currentPlayer = "X";
-		currentColor = "blue"
-	}
-}
+	function playerClickedCellFunc(){
+		if (this.innerHTML === ""){
+			this.innerHTML = currentPlayer;
+			this.style.color = currentColor;
+			var row = this.parentNode.id;
+			var col = this.id;
 
-function playerWon(){
-	var consecutiveSymbols = 0;
-	
-	// Check horizontals
-	for (var i = 0; i < board.length; i++){
-		for (var j = 0; j < board[0].length; j++){
-			if (board[i][j] === currentPlayer){
-				consecutiveSymbols++;
-			} else {
-				consecutiveSymbols = 0; 
-				break;
+			board[row][col] = currentPlayer;
+			if (playerWon()) {
+				alert("has won the game!!!");
+				return;
 			}
+			switchCurrentPlayer();
 		}
-		if (consecutiveSymbols === 3) return true;
 	}
-	
-	// Check verticals
-	for (var i = 0; i < board[0].length; i++){
-		for (var j = 0; j < board.length; j++){
-			if (board[j][i] === currentPlayer){
-				consecutiveSymbols++;
-			} else {
-				consecutiveSymbols = 0; 
-				break;
-			}
+
+	function switchCurrentPlayer(){
+		if (currentPlayer == "X"){
+			currentPlayer = "O";
+			currentColor = "red"
+		} else {
+			currentPlayer = "X";
+			currentColor = "blue"
 		}
-		if (consecutiveSymbols === 3) return true;
 	}
 	
-	// Check first diagonal
-	if ((board[0][0] === currentPlayer) && (board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) return true;
-	
-	// Check second diagonal
-	if (board[1][1] === currentPlayer){
-		if (board[0][2] === board[2][0] && board[0][2] == currentPlayer) return true;
+	function playerWon(){
+		
+		// Check horizontals
+		if (board[0][0] == currentPlayer && board[0][0] == board[0][1] && board[0][1] == board[0][2]) return true;
+		if (board[1][0] == currentPlayer && board[1][0] == board[1][1] && board[1][1] == board[1][2]) return true;
+		if (board[2][0] == currentPlayer && board[2][0] == board[2][1] && board[2][1] == board[2][2]) return true;
+
+		// Check verticals
+		if (board[0][0] == currentPlayer && board[0][0] == board[1][0] && board[1][0] == board[2][0]) return true;
+		if (board[0][1] == currentPlayer && board[0][1] == board[1][1] && board[1][1] == board[2][1]) return true;
+		if (board[0][2] == currentPlayer && board[0][2] == board[1][2] && board[1][2] == board[2][2]) return true;
+
+		// Check first diagonal
+		if (board[0][0] == currentPlayer && board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true;
+
+		// Check second diagonal
+		if (board[1][1] == currentPlayer && board[0][2] == board[2][0] && board[0][2] == board[1][1]) return true;
+		
+		return false;
 	}
 	
-	return false;
-}
+	return { play: initializeBoard };
+	
+})();
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	TicTacToe.play();
+});
